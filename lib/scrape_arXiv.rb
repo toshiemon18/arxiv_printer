43b7ecc865ca41bfc4a1858281095a9e362202fa
toshiemon18 <st12318@gmail.com>
2017-07-13 18:25:25 +0900

    Add description for ArXivPrinter::ScrapeArXiv methods

--- lib/scrape_arXiv.rb
+++ lib/scrape_arXiv.rb
@@ -21,17 +21,32 @@ module ArXivPrinter
       fetch_html
     end
 
+    # fetch target paper title
+    # === Args
+    #   None
+    # === Return
+    #   title(String) : paper title
     def fetch_paper_titile
       title = @html.xpath("//*[@id=\"abs\"]/div[2]/h1/text()").text.strip!
     end
 
+    # fetch pdf link for target paper
+    # === Args
+    #   None
+    # === Return
+    #   url(String) : pdf link
     def fetch_pdf_link
       base_url = "https://arxiv.org"
       href = @html.xpath("//*[@id=\"abs\"]/div[1]/div[1]/ul/li[1]/a").attribute("href").value
-      return "#{base_url}#{href}"
+      "#{base_url}#{href}"
     end
 
     private
+    # fetch html of arXiv page
+    # === Args
+    #   None
+    # === Return
+    #   None
     def fetch_html
       html = Nokogiri::HTML(open(@url, {proxy_http_basic_authentication: PROXY}))
       @html = html
