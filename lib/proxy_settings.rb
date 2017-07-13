62c289655b2ffc6efa909c81124411e8e4288ee6
toshiemon18 <st12318@gmail.com>
2017-07-13 21:39:48 +0900

    Add description for lib/proxy_settings.rb

--- lib/proxy_settings.rb
+++ lib/proxy_settings.rb
@@ -1,4 +1,11 @@
- module ArXivPrinter
+# coding : utf-8
+
+# ==============
+# Proxy Settings
+# ==============
+# Initialize array constant for TNCT proxy system
+
+module ArXivPrinter
   # This constant is the proxy parameters in TNCT proxy system
   PROXY = [
             ENV["PROXY_URL"],
