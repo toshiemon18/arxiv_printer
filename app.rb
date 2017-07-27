f2b11ddb598901fb55b6263e6f01b48c3a97c98b
toshiemon18 <st12318@gmail.com>
2017-07-27 11:43:41 +0900

    Rename to app.rb

---
+++ app.rb
@@ -0,0 +1,9 @@
+# coding : utf-8
+
+$:.unshift(File.join(File.dirname(__FILE__), "lib"))
+
+require "./lib/printer_helper"
+require "./lib/proxy_settings"
+require "./lib/scrape_arXiv"
+
+require "optparse"
