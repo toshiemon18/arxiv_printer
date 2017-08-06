cca048d561a72b9632b2a8ad788101fe709c44de
toshiemon18 <st12318@gmail.com>
2017-08-07 01:49:41 +0900

    Use dropbox api

--- lib/dropbox_helper.rb
+++ lib/dropbox_helper.rb
@@ -5,13 +5,15 @@
 # ==============
 # helper module for dropbox block
 
-require "dropbox-sdk"
+require "dropbox_api"
 require "open-uri"
 
 module ArXivPrinter
   class DropboxHelper
     def initialize
-
+      key = ENV["DROPBOX_KEY"]
+      secret_key= ENV["DROPBOX_SECRET_KEY"]
+      @client = DropboxApi
     end
   end
 end
