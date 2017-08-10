3a02f0763eb3a249f34c0af4d8d87bb1b4769dc4
toshiemon18 <st12318@gmail.com>
2017-08-11 00:45:10 +0900

    Enable proxy basic authentication

--- app.rb
+++ app.rb
@@ -16,11 +16,13 @@ module ArXivPrinter
     def options_define(argv=ARGV)
       @opt = OptionParser.new
       @options = {
+        url: ""
         use_proxy: false,
         use_ccpd:  false
       }
 
     def options_define
+      @opt.on("--url", "Target URL what arXiv page") {|v| @options[:url] = v}
       @opt.on("--use-proxy", "Use proxy server (default : false)") do |v|
         @options[:use_proxy] = v
       end
@@ -35,7 +37,18 @@ module ArXivPrinter
       rescue OptionParser::InvalidOption => e
         puts e.message
       end
-      @options
+      if opts[:url].empty?
+        raise "Invalid optional argument. --url is empty."
+      end
+      PrinterHelper.use_ccpd if opts[:use_ccpd]
+      args = {}
+      args[:authenticate_]
+      ArXivPrinter::Scrape.new(@options["url"])
     end
   end
+end
+
+if $0 == __FILE__
+  include ArXivPrinter
+  opts = CLI.run
 end
